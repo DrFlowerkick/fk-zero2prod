a2da290db9f81090a8296eb017dc52b3fbe74f82
DrFlowerkick <flowerkick14@gmail.com>
2023-12-03 21:25:14 +0100

    Refactoring health_check test to use ramdom port

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 //! main.rs
 
 use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("localhost:8000")
+        .expect("Failed to bind random port");
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,6 @@
 //! tests/health_check.rs
+
+use std::net::TcpListener;
 // `tokio::test` is the testing equivalent of `tokio::main`.
 // It also spares you from having to specify the `#[test]` attribute.
 //
@@ -6,14 +8,13 @@
 // `cargo expand --test health_check` (<- name of the test file)
 #[tokio::test]
 async fn health_check_works() {
-    // start our server app
-    spawn_app();
-    // We need to bring in `reqwest`
-    // to perform HTTP requests against our application.
+    // Arrange
+    let address = spawn_app();
+    // We need to bring in `reqwest` to perform HTTP requests against our application.
     let client = reqwest::Client::new();
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -23,7 +24,13 @@ async fn health_check_works() {
 }
 
 // Launch our application in the background
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind adress");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("localhost:0")
+        .expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    // We return the application address to the caller!
+    format!("http://localhost:{}", port)
 }
