eb89eba7f66330217a6a5fa166cdc1b911a12dc3
DrFlowerkick <flowerkick14@gmail.com>
2024-02-25 13:19:13 +0100

    Logging with log crate implemented

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 //! main.rs
 
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -7,12 +8,17 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
-    let address = format!("localhost:{}", configuration.application_port);
+    let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
