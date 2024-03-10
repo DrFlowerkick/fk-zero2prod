84a20adce1b933f20066efcef6c6fe40e77b5277
DrFlowerkick <flowerkick14@gmail.com>
2024-03-10 08:27:29 +0100

    Implemented basic setup for email client

---
+++ src/email_client.rs
@@ -0,0 +1,29 @@
+//! src/email_client.rs
+
+use crate::domain::SubscriberEmail;
+use reqwest::Client;
+
+pub struct EmailClient {
+    sender: SubscriberEmail,
+    http_client: Client,
+    base_url: String,
+}
+
+impl EmailClient {
+    pub fn new(base_url: String, sender: SubscriberEmail) -> Self {
+        Self {
+            sender,
+            http_client: Client::new(),
+            base_url,
+        }
+    }
+    pub async fn send_email(
+        &self,
+        recipient: SubscriberEmail,
+        subject: &str,
+        html_content: &str,
+        text_content: &str,
+    ) -> Result<(), String> {
+        todo!()
+    }
+}
