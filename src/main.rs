97fbebd25e0fe5993a0f5d2b50309524eeda541e
DrFlowerkick <flowerkick14@gmail.com>
2024-03-10 08:54:26 +0100

    Prepared test for sending an email

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 //! main.rs
 
 use sqlx::postgres::PgPoolOptions;
-use zero2prod::email_client::EmailClient;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
+use zero2prod::email_client::EmailClient;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
@@ -15,7 +15,10 @@ async fn main() -> Result<(), std::io::Error> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
-    let sender_email = configuration.email_client.sender().expect("Invalid sender email address.");
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
     let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
     let address = format!(
         "{}:{}",
--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,17 @@
 //! src/startup.rs
 
-use crate::routes::{health_check, subscribe};
 use crate::email_client::EmailClient;
+use crate::routes::{health_check, subscribe};
 use actix_web::{dev::Server, web, web::Data, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
-pub fn run(listener: TcpListener, db_pool: PgPool, email_client: EmailClient) -> Result<Server, std::io::Error> {
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+    email_client: EmailClient,
+) -> Result<Server, std::io::Error> {
     // Wrap the database pool and email client in a smart pointer
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
