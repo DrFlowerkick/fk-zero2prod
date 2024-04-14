af49fee2f698fee1692a2fdbc47d7cb52fff7aed
DrFlowerkick <flowerkick14@gmail.com>
2024-04-14 14:05:28 +0200

    Implemented session cookie with typed session.

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -1,12 +1,12 @@
 //! src/routes/admin/dashboard.rs
 
-use actix_session::Session;
 use actix_web::{http::header::ContentType, web, HttpResponse};
 use anyhow::Context;
 use sqlx::PgPool;
 use uuid::Uuid;
 
 use crate::error::Z2PResult;
+use crate::session_state::TypedSession;
 
 // Return an opaque 500 while preserving the error's root cause for logging.
 fn e500<T>(e: T) -> actix_web::Error
@@ -18,9 +18,9 @@ where
 
 pub async fn admin_dashboard(
     pool: web::Data<PgPool>,
-    session: Session,
+    session: TypedSession,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let username = if let Some(user_id) = session.get::<Uuid>("user_id").map_err(e500)? {
+    let username = if let Some(user_id) = session.get_user_id().map_err(e500)? {
         get_username(user_id, &pool).await.map_err(e500)?
     } else {
         todo!()
