2ec229ea8ed0779ba651035e423e922c086c66d0
DrFlowerkick <flowerkick14@gmail.com>
2024-04-14 13:21:47 +0200

    Implemented session cookie with simple String as key.

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -2,6 +2,7 @@
 
 use crate::authentication::{validate_credentials, Credentials};
 use crate::error::Error;
+use actix_session::Session;
 use actix_web::{error::InternalError, http::header::LOCATION, web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::Secret;
@@ -14,12 +15,13 @@ pub struct FormData {
 }
 
 #[tracing::instrument(
-    skip(form, pool),
+    skip(form, pool, session),
     fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
 )]
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
+    session: Session,
 ) -> Result<HttpResponse, InternalError<Error>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -29,17 +31,22 @@ pub async fn login(
     match validate_credentials(credentials, &pool).await {
         Ok(user_id) => {
             tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+            session.renew();
+            session
+                .insert("user_id", user_id)
+                .map_err(|e| login_redirect(Error::UnexpectedError(e.into())))?;
             Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/admin/dashboard"))
                 .finish())
         }
-        Err(e) => {
-            let e = Error::auth_error_to_login_error(e);
-            FlashMessage::error(e.to_string()).send();
-            let response = HttpResponse::SeeOther()
-                .insert_header((LOCATION, "/login"))
-                .finish();
-            Err(InternalError::from_response(e, response))
-        }
+        Err(e) => Err(login_redirect(Error::auth_error_to_login_error(e))),
     }
+}
+
+fn login_redirect(e: Error) -> InternalError<Error> {
+    FlashMessage::error(e.to_string()).send();
+    let response = HttpResponse::SeeOther()
+        .insert_header((LOCATION, "/login"))
+        .finish();
+    InternalError::from_response(e, response)
 }
